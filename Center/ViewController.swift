82b118e19374d0a6bc38f801bf8d6559e3ac2dcc
Mario <marioperozo@gmail.com>
2020-04-04 10:22:09 -0400

    Both Types of blue lines outlet and action plus vertical uistackview holding uilabel and unbutton

--- Center/ViewController.swift
+++ Center/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label1: UILabel!
+    var count: Int = 0;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        label1.backgroundColor = .systemPurple;
+        label1.textColor = .white;
     }
 
-
+    @IBAction func touchUpInside(_ sender: UIButton) {
+        count += Int.random(in: 12345 ... 67890);
+        label1.text = "You are visitor number \(count)!";
+        label1.backgroundColor = .red;
+    }
+    
 }
 
